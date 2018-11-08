5feb9c473acac0e422a2c36b20f82659923e94b8
Rominitq <sebas150698@gmail.com>
2018-11-07 20:41:43 -0500

    Version 7.7

--- Controllers/JuegosController.cs
+++ Controllers/JuegosController.cs
@@ -4,12 +4,20 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SICORA.Models;
 
 namespace SICORA.Controllers
 {
     public class JuegosController : Controller
     {
+        private readonly MvcContext _context;
+
+        public JuegosController(MvcContext context)
+        {
+            _context = context;
+        }
         public IActionResult Mostrar_juegos()
         {
             return View();
@@ -17,6 +25,20 @@ namespace SICORA.Controllers
         public IActionResult Agregar_juegos()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Agregar_juegos([Bind("Nom_juego, Desc_juego, trailer_juego, Img_juego, Pre_juego")] Juegos juegos)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _context.Add(juegos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Mostrar_juegos));
+            }
+            return View(juegos);
         }
 
     }
